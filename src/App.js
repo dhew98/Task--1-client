7304987b4515f94af8f25a638a1a902df6bccfab
Jawad Dhew <jawaddhew98@gmail.com>
2023-12-25 18:06:39 +0600

    form added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
-
-import React from 'react';
+import React, { useState } from 'react';
 import Swal from 'sweetalert2'
-
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
 
-    const buttonHandler=()=>{
+  
+
+    const handleSubmit=(event)=>{
       const currentDate = new Date().toISOString();
 
+
+      event.preventDefault();
+      const form = event.target;
+      const  userID= form.userID.value;
+      const deviceID = form.deviceID.value;
+      const queryText=form.queryText.value;
+
       const payload = {
-        userID: '1703152',
+        userID,
         date: currentDate,
-        deviceID: 'lenovo ideapad',
-        queryText: 'Need a new charger!',
+        deviceID,
+        queryText,
       };
 
 
@@ -31,12 +41,46 @@ function App() {
         .catch(error => {
           console.error(error);
         });
+        event.target.reset();
     }
    
 
   return (
     <div className="App">
-      <button type="text" onClick={buttonHandler}>Create Ticket</button>
+
+<Form onSubmit={handleSubmit} className=" w-25 mx-auto my-5">
+                       <Form.Group className="mb-3" controlId="formUserID">
+        <Form.Label>User ID</Form.Label>
+        <Form.Control
+          name="userID"
+          type="text"
+          placeholder="Enter User ID"
+          required
+        />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formDeviceID">
+        <Form.Label>Device ID</Form.Label>
+        <Form.Control
+          name="deviceID"
+          type="text"
+          placeholder="Enter Device ID"
+          required
+        />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formQueryText">
+        <Form.Label>Query Text</Form.Label>
+        <Form.Control
+          name="queryText"
+          as="textarea"
+          rows={3}
+          placeholder="Enter Query Text"
+          required
+        />
+      </Form.Group>
+      <Button type='submit' className='w-100 fw-bold' variant="primary" size="lg" >
+                            Create Ticket 
+      </Button>
+      </Form>
     </div>
   );
 }
